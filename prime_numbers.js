26d5bb372f31c1be71b8c708e3125f1d285b5413
khaled <awadkhaled.webdev@gmail.com>
2022-12-13 21:43:51 +0300

    200~prime numbers

---
+++ prime_numbers.js
@@ -0,0 +1,22 @@
+window.onload = function(){
+    for (var i = 2; i <= 30; i++) {
+      var a = 0;
+ 
+ 
+       for (var j = 2; j < i; j++) {
+           if (i % j == 0) {
+              a = 1;
+ 
+               break;
+ 
+           }
+       }
+       if (i > 1 && a== 0) {
+          console.log(i)
+         document.write(i+" ");
+       }
+ 
+   }
+ 
+ 
+ };
